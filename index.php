31604757733f349c306de1bd7aa02d333bf4c193
Susan Buck <susan@susanbuck.net>
2018-02-07 18:31:37 -0500

    Progress on foobooks0 post Part 5 video

--- index.php
+++ index.php
@@ -18,13 +18,38 @@ require 'logic.php';
 
 <h1>Foobooks0</h1>
 
-<?php foreach ($books as $title => $book): ?>
-    <div class='book'>
-        <div class='title'><?= $title ?></div>
-        <div class='author'>by <?= $book['author'] ?></div>
-        <img src='<?= $book['cover_url'] ?>' alt='Cover photo for the book <?= $title ?>'>
-    </div>
-<?php endforeach ?>
+<form method='GET' action='index.php'>
+
+    <label>Search for a book:
+        <input type='text' name='searchTerm' value='<?=sanitize($searchTerm)?>'>
+    </label>
+
+    <input type='submit' value='Search'>
+
+</form>
+
+<?php if($searchTerm): ?>
+    <p>You searched for <em><?=sanitize($searchTerm)?></em></p>
+<?php else: ?>
+    <p>Welcome to foobooks0; enter a title above to search our library</p>
+<?php endif; ?>
+
+<?php if($haveResults): ?>
+    <?php foreach ($books as $title => $book): ?>
+        <div class='book'>
+            <div class='title'><?= $title ?></div>
+            <div class='author'>by <?= $book['author'] ?></div>
+            <img src='<?= $book['cover_url'] ?>' alt='Cover photo for the book <?= $title ?>'>
+        </div>
+    <?php endforeach ?>
+
+<?php elseif($searchTerm): ?>
+    No results
+<?php endif; ?>
+
+<footer>
+    <a href='http://github.com/susanBuck/foobooks0'>View on Github</a>
+</footer>
 
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -2,3 +2,31 @@
 $booksJson = file_get_contents('books.json');
 
 $books = json_decode($booksJson, true);
+
+$haveResults = false;
+
+# Version 1
+//if (isset($_GET['searchTerm'])) {
+//    $searchTerm = $_GET['searchTerm'];
+//} else {
+//    $searchTerm = '';
+//}
+
+# Version 2 Using a ternary operator
+$searchTerm = isset($_GET['searchTerm']) ? $_GET['searchTerm'] : '';
+
+# Version 3 Using a null coalescing operator
+//$searchTerm = $_GET['searchTerm'] ?? '';
+
+if ($searchTerm) {
+    foreach ($books as $title => $book) {
+        if ($title != $searchTerm) {
+            unset($books[$title]);
+        }
+    }
+
+    if (count($books) > 0) {
+        $haveResults = true;
+    }
+}
+
